8c8bd42dea523e9f266526e18cbafb029086a461
bmallar <benmallar@gmail.com>
2024-05-01 15:58:41 -0500

    got all properties working with portfolio page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,10 +1,10 @@
 import Project from "./Project";
-// import image from "../../images"
-// import image from "../../images"
-// import image from "../../images"
-// import image from "../../images"
-// import image from "../../images"
-// import image from "../../images"
+import Trivl from "../../images/quiz-page-screenshot.jpg";
+import Activ from "../../images/activ-landing-page-screenshot.jpg";
+import Ai from "../../images/screenshot(28).png";
+import PWA from "../../images/screenshot(27).png";
+import EmployeeTracker from "../../images/screenshot(29).png";
+import Project3Name from "../../images/screenshot(30).png";
 
 export default function About() {
   const projects = [
@@ -12,7 +12,7 @@ export default function About() {
       key: 1,
       title: "Trivl",
       description: "This project entailed the creation of a trivia game in the style of the New York Times games Wordle, Connections, Crossword, etc.  Our game, TRIVL, is a ten question trivia game that allows users to test their knowledge across a wide variety of topics and compete with their friends in battles of wit.",
-      image: "../../images/",
+      image: Trivl,
       github: "https://github.com/njohnson2897/trivl-project",
       liveUrl: "https://njohnson2897.github.io/trivl-project/"
     },
@@ -20,7 +20,7 @@ export default function About() {
       key: 2,
       title: "Activ",
       description: "Activ is a workout tracking and sharing application that allows users to track past workouts, view other user's workouts for inspiration, and share accomplishments to their friends. The application has two major functions: first, Activ is meant to be an educational resource for athletes of any level to learn  more about exercises they already do or discover new exercises that they might like. Second, Activ is meant to serve as a social media platform through which friends can connect and celebrate the progress that they make.",
-      image: "../../images/",
+      image: Activ,
       github: "https://github.com/bmallar/Activ",
       liveUrl: "https://activ.onrender.com/"
     },
@@ -28,7 +28,7 @@ export default function About() {
       key: 3,
       title: "Ai Project",
       description: "This is a Ai prompt generator that will answer any coding question you might have. I created this generator outside of class using Javascript, Node and Api integration, the directions to run this are in my Readme",
-      image: "../../images",
+      image: Ai,
       github: "https://github.com/bmallar/Ai-Project",
       liveUrl: "https://github.com/bmallar/Ai-Project#"
     },
@@ -36,15 +36,15 @@ export default function About() {
       key: 4,
       title: "PWA Text Editor",
       description: "I made a PWA TextEditor to create notes or code snippets with or without an internet connection so that I can retrieve them for later use. Clicking the install button will install this as a application to your home screen",
-      image: "../../images",
+      image: PWA,
       github: "github repo url",
       liveUrl: "live url"
     },
     {
       key: 5,
       title: "Employee Tracker",
-      description: "This is a employee tracker made by using inquirer, postgres and SQL. This allows the user to  ",
-      image: "../../images",
+      description: "This is a employee tracker made by using inquirer, postgres and SQL. This allows the user to organize departments, roles and employees with unique properties within the postgres database",
+      image: EmployeeTracker,
       github: "https://github.com/bmallar/Employee-Tracker",
       liveUrl: "https://drive.google.com/file/d/1KX4SvbxbGGPrgvJToMTCYzKD0hbYz6kh/view"
     },
@@ -52,9 +52,9 @@ export default function About() {
       key: 6,
       title: "Project 3",
       description: "This is a placeholder for my 3rd project",
-      image: "../../images/placeholder",
-      github: "github repo url",
-      liveUrl: "live url"
+      image: Project3Name,
+      github: "https://github.com/bmallar",
+      liveUrl: "https://github.com/bmallar"
     },
   ]
 
@@ -62,14 +62,6 @@ export default function About() {
   return (
     <div>
       <h1>Portfolio</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
 
       {
         projects.map(project => {
